142e4c568dea22101a07a02945dee26d1333c46d
TonelliAngelo <56922400+TonelliAngelo@users.noreply.github.com>
2026-09-16 18:10:48 -0300

    Refactor gtag function and update comments
    Updated gtag function to use window.dataLayer and modified comments for clarity.

--- js/analytics.js
+++ js/analytics.js
@@ -3,24 +3,17 @@ const PROFACRIS_GA_ID = "G-BWMLRKPWWM";
 window.dataLayer = window.dataLayer || [];
 
 function gtag() {
-  dataLayer.push(arguments);
+  window.dataLayer.push(arguments);
 }
 
 window.gtag = gtag;
 
+// O gtag.js é carregado diretamente pelo <head> do index.html.
 gtag("js", new Date());
 
 gtag("config", PROFACRIS_GA_ID);
 
-const script = document.createElement("script");
-script.async = true;
-script.src =
-  "https://www.googletagmanager.com/gtag/js?id=" +
-  PROFACRIS_GA_ID;
-
-document.head.appendChild(script);
-
-// Mantém compatibilidade com os eventos personalizados do site.
+// Mantém os eventos personalizados já utilizados pelo site.
 window.profacrisTrack = function (eventName, details = {}) {
   gtag("event", eventName, details);
 };
